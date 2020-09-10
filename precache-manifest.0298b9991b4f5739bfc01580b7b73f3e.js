a358b2ad8c691b317366ddce234390d679d34aed
Sahithya <sahithyapatel5@gmail.com>
2020-09-09 23:31:30 -0700

    Updates

---
+++ precache-manifest.0298b9991b4f5739bfc01580b7b73f3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44700b9686a26260a7e682d944cccb72",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "38acc95875b5bc4515b0",
+    "url": "/project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c131e3be67513fa332ef",
+    "url": "/project/static/css/main.df25f01c.chunk.css"
+  },
+  {
+    "revision": "38acc95875b5bc4515b0",
+    "url": "/project/static/js/2.8d73cad2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/project/static/js/2.8d73cad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c131e3be67513fa332ef",
+    "url": "/project/static/js/main.a6771bc5.chunk.js"
+  },
+  {
+    "revision": "03fe14ff1ec9e62e46cc",
+    "url": "/project/static/js/runtime-main.74022d8a.js"
+  },
+  {
+    "revision": "8cb4028696a2ba58b8a530b79375ce12",
+    "url": "/project/static/media/fig.8cb40286.png"
+  }
+]);

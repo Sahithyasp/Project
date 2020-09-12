fa768c05df93b97f5a031a913d65605434e362ef
Sahithya <sahithyapatel5@gmail.com>
2020-09-12 14:17:35 -0700

    last

--- src/index.js
+++ src/index.js
@@ -431,7 +431,7 @@ class Project extends React.Component {
               background: "#00264d",
               height: 250,
               color: "white",
-              padding: 40,
+              padding: 45,
             }}
           >
             <br></br>{" "}
